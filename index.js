e5d6ddd157b22f7685a5e9a95c774255d2838150
trevorsmithdev <trevor.smith.dev@gmail.com>
2020-12-04 18:58:32 -0600

    update port

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const cookieParser = require('cookie-parser')
 require('dotenv').config()
 
 const app = express()
-const port = 3000
+const port = 3172
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
